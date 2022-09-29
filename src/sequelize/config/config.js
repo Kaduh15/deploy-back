4ba95fe32e96ca51cdd18e86aa3a2923eb8c5aee
kadu <kadu.silva2014@gmail.br>
2022-09-29 17:30:49 -0300

    configura db

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: 'postgres',
+    password: 'fsdfsdfsdfsdf343r3r2323',
+    database: 'postgres',
+    host: 'db.ljkhyjdrrbjqqbajlxvv.supabase.co',
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: 'postgres',
+    password: 'fsdfsdfsdfsdf343r3r2323',
+    database: 'postgres',
+    host: 'db.ljkhyjdrrbjqqbajlxvv.supabase.co',
     dialect: 'postgres',
   },
   production: {
